8458c1a6e77a108bccc351eb5e535188b8dd97d4
Poorvi-Sahyadri-ECE <shettypoorvi38@gmail.com>
2025-01-24 23:07:03 +0530

    Create iiitb_rv32.v

---
+++ Task4/iiitb_rv32.v
@@ -0,0 +1,86 @@
+LW  :EX_MEM_ALUOUT <= ID_EX_A + ID_EX_IMMEDIATE;
+    SW  :EX_MEM_ALUOUT <= ID_EX_IR[24:20] + ID_EX_IR[19:15];
+    endcase
+end
+
+BR_TYPE:begin
+    case(ID_EX_IR[14:12])
+    BEQ:begin 
+    EX_MEM_ALUOUT <= ID_EX_NPC+ID_EX_IMMEDIATE;
+    BR_EN <= 1'd1 ? (ID_EX_IR[19:15] == ID_EX_IR[11:7]) : 1'd0;
+    //BR_PC = EX_MEM_COND ? EX_MEM_ALUOUT : 1'd0; 
+end
+BNE:begin 
+    EX_MEM_ALUOUT <= ID_EX_NPC+ID_EX_IMMEDIATE;
+    BR_EN <= (ID_EX_IR[19:15] != ID_EX_IR[11:7]) ? 1'd1 : 1'd0;
+end
+endcase
+end
+
+SH_TYPE:begin
+case(ID_EX_IR[14:12])
+SLL:EX_MEM_ALUOUT <= ID_EX_A << ID_EX_B;
+SRL:EX_MEM_ALUOUT <= ID_EX_A >> ID_EX_B;
+endcase
+end
+
+endcase
+end
+
+
+//EXECUTION STAGE END
+		
+//MEMORY STAGE
+always@(posedge clk) begin
+
+MEM_WB_IR <= EX_MEM_IR;
+
+case(EX_MEM_IR[6:0])
+
+AR_TYPE:MEM_WB_ALUOUT <=  EX_MEM_ALUOUT;
+SH_TYPE:MEM_WB_ALUOUT <=  EX_MEM_ALUOUT;
+
+M_TYPE:begin
+case(EX_MEM_IR[14:12])
+LW:MEM_WB_LDM <= DM[EX_MEM_ALUOUT];
+SW:DM[EX_MEM_ALUOUT]<=REG[EX_MEM_IR[11:7]];
+endcase
+end
+
+endcase
+end
+
+// MEMORY STAGE END
+
+
+//WRITE BACK STAGE
+always@(posedge clk) begin
+
+case(MEM_WB_IR[6:0])
+
+AR_TYPE:begin 
+WB_OUT<=MEM_WB_ALUOUT;
+REG[MEM_WB_IR[11:7]]<=MEM_WB_ALUOUT;
+end
+
+SH_TYPE:begin
+WB_OUT<=MEM_WB_ALUOUT;
+REG[MEM_WB_IR[11:7]]<=MEM_WB_ALUOUT;
+end
+
+M_TYPE:begin
+case(MEM_WB_IR[14:12])
+LW:begin
+WB_OUT<=MEM_WB_LDM;
+REG[MEM_WB_IR[11:7]]<=MEM_WB_LDM;
+end
+endcase
+end
+
+
+
+endcase
+end
+//WRITE BACK STAGE END
+
+endmodule
